107d0a5c210df5ba147aa68cd910a19fd327157c
Amitkjha28 <103565661+Amitkjha28@users.noreply.github.com>
2022-06-15 06:20:07 +0530

    destination

--- myapp/urls.py
+++ myapp/urls.py
@@ -7,7 +7,8 @@ from myapp import views
 
 urlpatterns = [
     #path('', views.myindex),
-    path('', views.myindex,name='myhome'),
+    path('', views.my_index,name='myhome'),
+    path('city',views.my_destination,name='mydestination')
 
 ]#+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 
--- myapp/views.py
+++ myapp/views.py
@@ -2,5 +2,12 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-def myindex(request):
+from myapp.models import District
+
+
+def my_index(request):
     return render(request,"tourism.html")
+
+def my_destination(request):
+    district_data = District.objects.all()
+    return  render(request,"Destination.html",{ 'districtdata' :district_data})
