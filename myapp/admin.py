c8d38ef462442f4aa460de05add515e2a3b5fbf8
Amitkjha28 <103565661+Amitkjha28@users.noreply.github.com>
2022-06-23 03:31:48 +0530

    booking admin register

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from myapp.models import District, Hotel
+from myapp.models import District, Hotel, Booking
 
 
 @admin.register(District)
@@ -9,5 +9,9 @@ class DistrictAdmin(admin.ModelAdmin):
     list_display = ['district_name','district_info','district_reach','district_things']
 
 @admin.register(Hotel)
-class DistrictAdmin(admin.ModelAdmin):
+class HotelAdmin(admin.ModelAdmin):
     list_display = ['district','hotel_name','hotel_Image','price','discount_percent']
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ['hotel_name','check_in','check_out','room','adult','child']
